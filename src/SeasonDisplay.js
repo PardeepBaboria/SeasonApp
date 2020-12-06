94c59ab28a248f359d9120ce8fd2d8c16ef4f45f
PardeepBaboria <pardeepbaboria@gmail.com>
2020-12-06 13:45:07 +0530

    Initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,33 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, it's chilly!",
+        iconName: "snowflake"
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month < 2 && month < 10)
+        return lat > 0 ? "winter" : "summer";
+    return lat > 0 ? "winter" : "summer";
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`left-icon massive ${iconName} icon`}></i>
+            <h1>{text}</h1>
+            <i className={`right-icon massive ${iconName} icon`}></i>
+        </div>
+    )
+}
+
+export default SeasonDisplay;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+
+    state = { lat: null, errorMessage: "" };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            success => this.setState({ lat: success.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
+        );
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat)
+            return <div> Error: {this.state.errorMessage} </div>
+        if (this.state.lat && !this.state.errorMessage)
+            return <SeasonDisplay lat={this.state.lat} />
+        return <Spinner message = "Please accept loacation request..."/>
+    }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
